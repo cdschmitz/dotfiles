0b10ae477afe06b09399eaa735262ca3e7f98469
Chris Schmitz <Christopher_Schmitz@mentor.com>
2015-08-30 18:52:38 -0700

    osx, utils

---
+++ create_symbolic_links.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+    && source 'utils.sh'
+
+declare -a FILES_TO_SYMLINK=(
+
+    '.aliases'
+    '.bash_profile'
+    '.bash_prompt'
+    '.bashrc'
+    '.exports'
+    '.functions'
+    '.inputrc'
+
+    '.git-completion.bash'
+    '.gitconfig'
+    '.gitconfig.local'
+    '.gitignore_global'
+
+    '.vim'
+    '.vimrc'
+
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    local i=''
+    local sourceFile=''
+    local targetFile=''
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(cd .. && pwd)/$i"
+        targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+}
+
+main
